02549ca2f8a7e7a4804699612fc257b372d2f325
Surya Narayan Dash <surya.dash0967@gmail.com>
2025-02-25 17:07:39 +0530

    more responsiveness added

--- src/components/Drip.jsx
+++ src/components/Drip.jsx
@@ -2,25 +2,27 @@ export default function Drip() {
     return (
         <>
             <div className="drip-wrapper">
-                <div id="drip" style={{margin: "3.5rem 0"}}></div>
+                <div id="drip" style={{ margin: "3.5rem 0" }}></div>
                 <div className="drip-seg-wrapper" style={{}}>
-                    <div className="drip-seg">
-                        <h1 style={{marginBottom: "2rem", fontFamily: "Clash Display", fontWeight: 500}}>The Drip</h1>
-                        <div className="drip-desc">
-                            <div className="dress-wrapper">
-                                <img src="/images/formals.png" alt="" />
-                                <div>Formal shirt and pant</div>
+                    <div className="drip-seg-head">
+                        <div className="drip-seg">
+                            <h1 style={{ marginBottom: "2rem", fontFamily: "Clash Display", fontWeight: 500 }}>The Drip</h1>
+                            <div className="drip-desc">
+                                <div className="dress-wrapper">
+                                    <img src="/images/formals.png" alt="" />
+                                    <div>Formal shirt and pant</div>
+                                </div>
+                            </div>
+                            <div className="drip-desc">
+                                <div className="dress-wrapper">
+                                    <img src="/images/saree.png" alt="" />
+                                    <div>Traditional Saree</div>
+                                </div>
                             </div>
                         </div>
-                        <div className="drip-desc">
-                            <div className="dress-wrapper">
-                                <img src="/images/saree.png" alt="" />
-                                <div>Traditional Saree</div>
-                            </div>
+                        <div>
+                            <img className="drip-img" src="/images/drip-img.jpeg" alt="" />
                         </div>
-                    </div>
-                    <div>
-                        <img className="drip-img" src="/images/drip-img.jpeg" alt="" />
                     </div>
                 </div>
             </div>
